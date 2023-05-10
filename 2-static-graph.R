e18b5e0829ca628713c8d28bd1081604bdf9537e
an-bui <a.thanh.bui@gmail.com>
2023-05-09 23:28:29 -0700

    changed jitter to be geom_point with position_jitter

--- 2-static-graph.R
+++ 2-static-graph.R
@@ -16,14 +16,14 @@ lobs <- readRDS(file = here::here("data", "lobsters_locations.rds"))
 lobs_summary <- lobs %>% 
   
   # calculate total lobster counts by protection status, site, & year (each point will represent lobster counts at a single site for each year from 2012-2018) ----
-group_by(protection_status, site, year) %>% 
+  group_by(protection_status, site, year) %>% 
   count() %>% 
   
   # create a new column called "marker" - this will show up later! ----
   mutate(marker = paste("Site:", site, "<br>",
-                        "Year:", year, "<br>",
-                        "Status:", protection_status, "<br>",
-                        "Lobster count:", n))
+                      "Year:", year, "<br>",
+                      "Status:", protection_status, "<br>",
+                      "Lobster count:", n))
 
 #.........................create boxplot.........................
 static <- lobs_summary %>% 
@@ -31,13 +31,15 @@ static <- lobs_summary %>%
   # create boxplot of mpa vs non-mpa lobster counts ----
   ggplot(aes(x = protection_status, y = n)) +
   geom_boxplot(width = 0.5, outlier.shape = NA) +
-  geom_jitter(width = 0.25, size = 4, alpha = 0.8, aes(color = site, shape = site)) +
+  geom_point(aes(color = site, shape = site), size = 4, alpha = 0.8, 
+             # turn the points into a jitter (with a little more control than geom_jitter)
+             position = position_jitter(width = 0.25, height = 0, seed = 1)) +
   
   # update colors ----
   scale_color_manual(values = c("#91B38A", "#9565CC", "#CCC065", "#658ACC", "#CC6565")) +
   scale_shape_manual(values = c(15, 25, 17, 18, 19)) +
   
-  # update labels
+  # update labels ----
   labs(x = "Protection Status",
        y = "Lobster Counts",
        color = "Site", 
