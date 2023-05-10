c8cbff557077758cfd2d3f0e8e4b457776b26ba5
an-bui <a.thanh.bui@gmail.com>
2023-05-09 23:28:36 -0700

    fixed the boxplot issue

--- 3-plotly.R
+++ 3-plotly.R
@@ -20,27 +20,29 @@ ggplotly(static)
 static_with_marker <- lobs_summary %>% 
   
   # create boxplot of mpa vs non-mpa lobster counts ----
-ggplot(aes(x = protection_status, y = n)) +
+  ggplot(aes(x = protection_status, y = n, text = marker, group = protection_status)) +
   geom_boxplot(width = 0.5, outlier.shape = NA) +
-  geom_jitter(width = 0.25, size = 4, alpha = 0.8, aes(color = site, shape = site)) +
+  geom_point(aes(color = site, shape = site), size = 4, alpha = 0.8, 
+             position = position_jitter(width = 0.25, height = 0, seed = 1)) +
   
   # update colors ----
-scale_color_manual(values = c("#91B38A", "#9565CC", "#CCC065", "#658ACC", "#CC6565")) +
+  scale_color_manual(values = c("#91B38A", "#9565CC", "#CCC065", "#658ACC", "#CC6565")) +
   scale_shape_manual(values = c(15, 25, 17, 18, 19)) +
   
-  # update labels
+  # update labels ----
   labs(x = "Protection Status",
        y = "Lobster Counts",
        color = "Site", 
        shape = "Site") + 
   
   # theme ----
-theme_linedraw() +
+  theme_linedraw() +
   theme(axis.text = element_text(size = 10),
         axis.title = element_text(size = 13),
         legend.text = element_text(size = 10),
         legend.title = element_text(size = 11))
 
+# running the ggplot object will give you a scary warning - that's ok!
 static_with_marker
 
 ggplotly(static_with_marker, tooltip = "text")
