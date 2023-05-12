251e0dea4c5b9f7870161f5f48cbb0585c7b2371
samanthacsik <samanthacsik@gmail.com>
2023-05-12 09:28:21 -0700

    fixed merge conflict, pushing latest leaflet map

--- X-leaflet-map.R
+++ X-leaflet-map.R
@@ -5,37 +5,44 @@
 
 #.........................load libraries.........................
 library(leaflet)
+library(tidyverse)
 
 #..........................read in data..........................
 lobs <- readRDS(file = here::here("data", "lobsters_locations.rds"))
 
+#..................get unique site/location data.................
+sites <- lobs |> 
+  select(site, protection_status, lat, lon) |> 
+  distinct()
+  
 #.......................create custom icon.......................
 lobster_icon <- makeIcon(
-  iconUrl = "media/lobster2.png",
-  iconWidth = 20, iconHeight = 20
+  iconUrl = "media/lobster.png",
+  iconWidth = 30, iconHeight = 30
 )
 
 #...........................create map...........................
 
 # initialize map ----
-site_map <- leaflet() |> 
+site_map <- leaflet(data = sites) |> 
   
   # add base map tiles (use `addTiles()` for Google maps tiles, OR `addProviderTiles()` for 3rd party base maps: https://leaflet-extras.github.io/leaflet-providers/preview/) ----
-  addProviderTiles(providers$Esri.WorldImagery, group = "ESRI World Imagery") |> # modify base map appearance with options: `providerTileOptions(opacity = 0.5, noWrap = FALSE)`
+  addProviderTiles(providers$Esri.WorldImagery, group = "ESRI World Imagery",
+                   options = providerTileOptions(noWrap = FALSE)) |> # modify base map appearance with options: `providerTileOptions(opacity = 0.5, noWrap = FALSE)`
   addProviderTiles(providers$Esri.OceanBasemap, group = "ESRI Oceans") |> 
-  
-  # set view over Santa Barbara Channel ----
-  setView(lng =  -119.8276, lat = 34.4358, zoom = 9) |> 
   
   # add mini map
   addMiniMap(toggleDisplay = TRUE, minimized = TRUE) |> 
   
-  # add markers - CURRENTLY SOOOO SLOW
-  # addMarkers(data = lobs, #icon = lobster_icon,
-  #            lng = ~lon, lat = ~lat,
-  #            label = paste("Site Name:", lobs$site, "<br>",
-  #                          "Coorinates (lat/long):", lobs$lat, ",", lobs$lon)) |>
+  # set view over Santa Barbara Channel ----
+  setView(lng =  -119.8276, lat = 34.4358, zoom = 9) |> 
   
+  # add markers
+  addMarkers(icon = lobster_icon,
+             lng = ~lon, lat = ~lat,
+             popup = paste("Site Name:", sites$site, "<br>",
+                           "Coorinates (lat/long):", sites$lat, ",", sites$lon)) |>
+
   # add layers control (toggle base map tiles & markers based on group IDs)
   addLayersControl(
     baseGroups = c("ESRI World Imagery", "ESRI Oceans")
